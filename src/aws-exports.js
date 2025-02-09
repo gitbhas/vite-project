27b02c138e3cef3c84e77c54ec27c9e7ab0e0675
Murthy Pothapragada <Murthy.Pothapragada@gdit.com>
2025-02-08 23:12:02 -0500

    package gen

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,8 +2,8 @@
 const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "YOUR_USER_POOL_ID",
-    "aws_user_pools_web_client_id": "YOUR_APP_CLIENT_ID",
+    "aws_user_pools_id": "us-east-1_8Y2ZD07f1",
+    "aws_user_pools_web_client_id": "3ij75agq13fhrq34fhqequsn5b",
     "oauth": {}
 };
 
