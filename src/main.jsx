e90fb1a77dc9aa82c18e4990ef01363699638024
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-11 18:31:43 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,9 @@ import { AuthProvider } from './components/AuthContext'
 Amplify.configure(awsconfig)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ 
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
+ 
 )
