5e41b37b7b33e8e00e1f9e585fcd0f3f5c995d3f
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-11 18:41:08 -0500

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,7 +3,7 @@ const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_8FyE7Hy4o",
-    "aws_user_pools_web_client_id": "4dhskmimg30rr0efqkajgj6nm7",
+    "aws_user_pools_web_client_id": "m4js7ltsb49qcp2mmb42835ir",
     "oauth": {}
 };
 
