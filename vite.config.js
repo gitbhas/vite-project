a7d6b3607160bc20c5351ef0eb7b9029294382d8
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-17 09:59:40 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom']
-        }
-      }
-    }
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: true
   },
   server: {
-    port: 3000,
-    host: true
+    port: 3000
   }
 })
