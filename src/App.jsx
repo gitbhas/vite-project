df70e425b2686e93d0e84da5b80172994a1f8ce2
gitbhas <36489260+gitbhas@users.noreply.github.com>
2025-02-20 21:21:05 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // FileUpload.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import './App.css';
+import '../src/App.css';
 
 const FileUpload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
